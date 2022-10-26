4e0e7500302581ec9e15c8e5257e97a659229b92
Juan Tapia <tapiahuanca.juanluis@gmail.com>
2022-10-25 22:10:36 -0300

    poc-jersey-crud-usuarios add fix

---
+++ poc-jersey-crud-usuarios/src/main/java/com/github/sanchezih/jersey/Configuration.java
@@ -0,0 +1,12 @@
+package com.github.sanchezih.jersey;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class Configuration extends Application {
+
+}
